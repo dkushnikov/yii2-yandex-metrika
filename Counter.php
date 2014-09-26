dd98aae9ecc0fcecf398a6a88dd2763dec1a433d
Dmitry Kushnikov <dkushnikov@gmail.com>
2014-09-26 17:53:06 +0400

    Widget placeholder

---
+++ Counter.php
@@ -0,0 +1,20 @@
+<?php
+namespace yii\widgets;
+
+use yii\base\Widget;
+
+class Counter extends Widget
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        parent::run();
+
+    }
+
+
+} 
